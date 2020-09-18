2803549285a5820822e4f85dfd125409ca698bb2
Aidan Middleton <aidanemiddleton@gmail.com>
2020-09-18 09:57:21 -0700

    first commit

---
+++ arrayExample.js
@@ -0,0 +1,11 @@
+let cars = ['BMW', 'Audi', 'Ford'];
+
+console.log(cars[0]);
+
+console.log(cars[2]);
+
+console.log(cars[3]);
+
+cars.push('Dodge')
+
+console.log(cars[3]);
---
+++ loopingThroughArrays.js
@@ -0,0 +1,9 @@
+let foods = ['apple', 'bread', 'cheese', 'wine'];
+
+console.log(foods[2]);
+
+console.log(foods.length);
+
+for (let i = 0; i < foods.length; i++) {
+  console.log(`${foods[i]} is tasty`);
+};
